b7c3a80af6d3b86fac55090db5e5e064e6bd5486
Ergon Zamarian Lima <63436543+ergonlima@users.noreply.github.com>
2020-10-15 21:58:39 -0400

    Inserindo métodos no Controller e efetuando teste

--- Rastreamento/src/controller/RastreamentoC.java
+++ Rastreamento/src/controller/RastreamentoC.java
@@ -1,5 +1,127 @@
 package controller;
 
+
+import java.sql.ResultSet;
+
+import model.RastreamentoM;
+
+
 public class RastreamentoC {
+	
+	// acesso ao bd manipular dml
+	BancoDados bd = new BancoDados();
+	//ResultSet para receber dados de consulta
+	ResultSet dados;
+	boolean del;
+	
+	public void cadastroRastreio(RastreamentoM rastreio) {
+		
+		try {
+			
+			bd.conexao();
+			
+			
+			String sql = "insert into rastreamento values ('"+rastreio.getIdRastreio()+"','"+rastreio.getEqunr()+"','"+rastreio.getCentro_custo()+"','"+rastreio.getDataEntrada()+"','"+rastreio.getLocalidade()+"')";
+			bd.getStatement().execute(sql);
+			
+			
+			//Aviso cadastro com sucesso
+			
+			javax.swing.JOptionPane aviso = new javax.swing.JOptionPane();
+			aviso.showMessageDialog(null, "Rastreio cadastrado");
+			
+			
+			bd.desconecta();
+			
+		}catch(Exception erro) {
+			erro.printStackTrace();
+			
+			javax.swing.JOptionPane aviso = new javax.swing.JOptionPane();
+			aviso.showMessageDialog(null, "ERRO");
+		}
+		
+	}
+	public void ImpressaoTodosRastreios() {
+		
+		try {
+			
+			bd.conexao();
+			String sql = "select * from rastreamento";
+			
+			dados = bd.getStatement().executeQuery(sql);
+			
+			//impressão dos dados no prompt
+			while(dados.next()) {
+				System.out.println("----------------------------------------------------");
+				System.out.println("Id do Rastreamento:         "+ dados.getInt(1));
+				System.out.println("Equnr (N° Ativo):           "+ dados.getString(2));
+				System.out.println("Centro de Custo:            "+ dados.getString(3));
+				System.out.println("Data de Entrada Localidade: "+ dados.getString(4));
+				System.out.println("Localidade Ativo:           "+ dados.getString(5));
+				
+			}
+			
+			bd.desconecta();
+			
+		}catch(Exception erro) {
+			
+			erro.printStackTrace();
+		}
+	}
+	public void BuscaPorIdRastreio(int idRastreio) {
+		
+		try {
+			bd.conexao();
+			String sql = "select * from rastreamento where idRastreio = '"+idRastreio+"'";
+			dados = bd.getStatement().executeQuery(sql);
+			
+			
+			while(dados.next()) {
+				System.out.println("----------------------------------------------------");
+				System.out.println("Id do Rastreamento:         "+ dados.getInt(1));
+				System.out.println("Equnr (N° Ativo):           "+ dados.getString(2));
+				System.out.println("Centro de Custo:            "+ dados.getString(3));
+				System.out.println("Data de Entrada Localidade: "+ dados.getString(4));
+				System.out.println("Localidade Ativo:           "+ dados.getString(5));
+			}
+			
+			
+			bd.desconecta();
+			
+			
+		}catch(Exception erro) {
+			erro.printStackTrace();
+		}
+		
+	}
+	
+	public void ExclusaoRastreioPorId(int idRastreio) {
+		
+		try {
+			bd.conexao();
+			
+			String sql = "select * from rastreamento where idRastreio = '"+idRastreio+"'";
+			dados = bd.getStatement().executeQuery(sql);
+			
+			//impressão dos dados no prompt
+			while(dados.next()) {
+				System.out.println("----------------------------------------------------");
+				System.out.println("Id do Rastreamento:         "+ dados.getInt(1));
+				System.out.println("Equnr (N° Ativo):           "+ dados.getString(2));
+				System.out.println("Centro de Custo:            "+ dados.getString(3));
+				System.out.println("Data de Entrada Localidade: "+ dados.getString(4));
+				System.out.println("Localidade Ativo:           "+ dados.getString(5));
+			}
+			
+			String sql_1 = "delete from rastreamento where idRastreio = '"+idRastreio+"'";
+			del = bd.getStatement().execute(sql_1);
+			
+			
+			bd.desconecta();
+			
+		}catch(Exception erro) {
+			erro.printStackTrace();
+		}
+	}
 
 }
--- Rastreamento/src/principal/Main.java
+++ Rastreamento/src/principal/Main.java
@@ -1,9 +1,37 @@
 package principal;
 
+import controller.RastreamentoC;
+import model.RastreamentoM;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		// TESTES - Basta retirar o comentário da linha desejada
+		
+		RastreamentoM rastrar1 = new RastreamentoM(1, "H1LK2654", "Unid Energisa CG", "20/05/2019", "Campo Grande");
+		RastreamentoM rastrar2 = new RastreamentoM(2, "LO5KD6FD", "Unid Energisa AC", "20/05/2019", "Águas Claras");
+		RastreamentoM rastrar3 = new RastreamentoM(3, "SOEPT165", "Unid Energisa Ds", "20/05/2019", "Dourados");
+		RastreamentoM rastrar4 = new RastreamentoM(4, "WQ62RT65", "Unid Energisa CA", "20/05/2019", "Cuiaba");
+		
+		RastreamentoC efetuar = new RastreamentoC();
+		
+		// TESTE CADASTRO
+		//efetuar.cadastroRastreio(rastrar1);
+		//efetuar.cadastroRastreio(rastrar2);
+		//efetuar.cadastroRastreio(rastrar3);
+		//efetuar.cadastroRastreio(rastrar4);
+		
+		// TESTE IMPRIME TODOS OS RASTREIOS
+		//efetuar.ImpressaoTodosRastreios();
+		
+		// TESTE BUSCA POR ID DE RASTREIO
+		//efetuar.BuscaPorIdRastreio(1);
+		
+		// TESTE EXCLUSÃO
+		//efetuar.ExclusaoRastreioPorId(4);
+		
 
 	}
 
